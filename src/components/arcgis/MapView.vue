178e8ea047b400a8794101b3bdc87dc4c58d5def
阳文虎 <yangwh@geoscene.cn>
2023-07-03 23:55:32 +0800

    config🔧:

---
+++ src/components/arcgis/MapView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+import MapView from '@arcgis/core/views/MapView.js'
+
+defineProps({
+  map: Object,
+})
+
+onMounted(() => {
+  const view = new MapView({
+    map: props.map,
+    container: document.createElement('div'),
+  })
+  view.when(() => {
+    view.goTo({
+      center: [-118.805, 34.027],
+      zoom: 13,
+    })
+  })
+})
+</script>
+
+<style scoped></style>
--- src/views/HomeView/ViewContainer.vue
+++ src/views/HomeView/ViewContainer.vue
@@ -1,13 +1,6 @@
 <template>
-  <win-box default-direction="vertical" class="bg-red-500">
+  <win-box>
     <div class="bg-dark-500">bg-dark-500</div>
-    <win-box>
-      <div />
-      <win-box default-direction="vertical">
-        <div />
-        <div />
-      </win-box>
-    </win-box>
     <div class="bg-green-500" />
   </win-box>
 </template>
